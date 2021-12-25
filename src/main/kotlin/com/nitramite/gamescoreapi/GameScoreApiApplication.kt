a6d3cb79b2c27c673c597dee5944311336ce992b
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-25 18:08:37 +0200

    add project template

---
+++ src/main/kotlin/com/nitramite/gamescoreapi/GameScoreApiApplication.kt
@@ -0,0 +1,11 @@
+package com.nitramite.gamescoreapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameScoreApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GameScoreApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/nitramite/gamescoreapi/GameScoreApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nitramite.gamescoreapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameScoreApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
