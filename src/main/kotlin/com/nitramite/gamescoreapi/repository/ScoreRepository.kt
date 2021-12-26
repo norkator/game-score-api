652ec19b4a4f8ffc821c8afa09af1d644dedb670
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-26 23:07:46 +0200

    add findAllByName for score repository interface

--- src/main/kotlin/com/nitramite/gamescoreapi/repository/ScoreRepository.kt
+++ src/main/kotlin/com/nitramite/gamescoreapi/repository/ScoreRepository.kt
@@ -3,8 +3,13 @@ package com.nitramite.gamescoreapi.repository
 import com.nitramite.gamescoreapi.dao.ScoreEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 import javax.transaction.Transactional
 
 @Repository
 @Transactional
-interface ScoreRepository : CrudRepository<ScoreEntity, Long>
+interface ScoreRepository : CrudRepository<ScoreEntity, Long> {
+
+    fun findAllByName(name: String): Optional<ScoreEntity>
+
+}
