0699bc148fb51c371a2f4ef0af180aea7ad1a946
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-26 22:39:44 +0200

    score model data access object implementation

--- src/main/kotlin/com/nitramite/gamescoreapi/model/Score.kt
+++ src/main/kotlin/com/nitramite/gamescoreapi/model/Score.kt
@@ -1,9 +1,15 @@
 package com.nitramite.gamescoreapi.model
 
-class Score {
+import com.nitramite.gamescoreapi.dao.ScoreEntity
 
-    val id: Int = 0
-    var name: String? = null
-    var score: Int = 0
-
+class Score(
+    val id: Int = 0,
+    var name: String? = null,
+    var score: Int = 0,
+) {
+    fun fromDao(scoreEntity: ScoreEntity) = Score(
+        scoreEntity.id,
+        scoreEntity.name,
+        scoreEntity.score
+    )
 }
