6caa3513071b746564adb572577b912a697a7dce
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-27 13:51:27 +0200

    modify migration to create proper constraints

--- src/main/resources/db/migration/V1_0001__init_tables.sql
+++ src/main/resources/db/migration/V1_0001__init_tables.sql
@@ -1,6 +1,7 @@
+/* Create tables */
 CREATE TABLE game
 (
-    id          BIGINT,
+    id          BIGINT UNIQUE,
     name        VARCHAR,
     description TEXT,
     enabled     BOOLEAN DEFAULT true
@@ -35,7 +36,29 @@ CREATE TABLE achievement
 
 CREATE TABLE achievementType
 (
-    id          BIGINT,
+    id          BIGINT UNIQUE,
     name        VARCHAR,
     description TEXT
 );
+
+
+/* Create constraints */
+alter table public.score
+    add constraint score_game_id_fk
+        foreign key (gameid) references public.game (id);
+
+alter table public.score
+    add constraint score_client_clientuid_fk
+        foreign key (clientuid) references public.client (clientuid);
+
+alter table public.achievement
+    add constraint achievement_game_id_fk
+        foreign key (gameid) references public.game (id);
+
+alter table public.achievement
+    add constraint achievement_client_clientuid_fk
+        foreign key (clientuid) references public.client (clientuid);
+
+alter table public.achievement
+    add constraint achievement_achievementtype_id_fk
+        foreign key (achievementtypeid) references public.achievementtype (id);
