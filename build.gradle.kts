54b45d09caeeb69315c1d0cab4064a3e6b3bd494
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-27 21:16:48 +0200

    add embedded-database-spring-test test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.zonky.test:embedded-database-spring-test:2.1.1")
 }
 
 tasks.withType<KotlinCompile> {
--- src/test/kotlin/com/nitramite/gamescoreapi/GameScoreApiApplicationTests.kt
+++ src/test/kotlin/com/nitramite/gamescoreapi/GameScoreApiApplicationTests.kt
@@ -1,9 +1,11 @@
 package com.nitramite.gamescoreapi
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@AutoConfigureEmbeddedDatabase
 class GameScoreApiApplicationTests {
 
 	@Test
