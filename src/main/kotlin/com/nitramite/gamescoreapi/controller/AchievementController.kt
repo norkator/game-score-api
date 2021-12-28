44fd4f6aff80e995401d45de5c50b344c02caee1
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-28 13:30:11 +0200

    finalise AchievementController

--- src/main/kotlin/com/nitramite/gamescoreapi/controller/AchievementController.kt
+++ src/main/kotlin/com/nitramite/gamescoreapi/controller/AchievementController.kt
@@ -2,30 +2,37 @@ package com.nitramite.gamescoreapi.controller
 
 import com.nitramite.gamescoreapi.model.Achievement
 import com.nitramite.gamescoreapi.model.AchievementType
+import com.nitramite.gamescoreapi.service.AchievementService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/achievements")
 class AchievementController(
-    // private val clientService: ClientService,
+    private val achievementService: AchievementService,
 ) {
 
     @PostMapping("/achievementType")
     fun postAchievementType(@RequestBody achievementType: AchievementType): AchievementType? {
-        // return achievementService.addAchievementType(achievementType...)
-        return null
+        return achievementService.addAchievementType(achievementType.name, achievementType.description)
     }
 
     @PostMapping("/achievement")
     fun postAchievement(@RequestBody achievement: Achievement): Achievement? {
-        // return achievementService.addAchievement(achievement...)
-        return null
+        return achievementService.addAchievement(
+            achievement.gameId,
+            achievement.clientUid,
+            achievement.achievementTypeId
+        )
     }
 
-    @GetMapping("/achievementByGameIdAndClientUid")
+    @GetMapping("/achievementsByGameId")
+    fun getAchievementsByGameIdAndClientUid(@RequestParam gameId: Long): List<Achievement>? {
+        return achievementService.getAchievementsByGameId(gameId)
+    }
+
+    @GetMapping("/achievementsByGameIdAndClientUid")
     fun getAchievementsByGameIdAndClientUid(@RequestParam gameId: Long, clientUid: String): List<Achievement>? {
-        // return achievementService.addAchievement(achievement...)
-        return null
+        return achievementService.getAchievementsByGameIdAndClientUid(gameId, clientUid)
     }
 
 }
