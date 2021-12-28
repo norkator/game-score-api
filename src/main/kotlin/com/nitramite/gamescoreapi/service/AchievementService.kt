6b240099ed231978a91adb470152394c4c64f8b3
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-12-28 13:29:59 +0200

    Create AchievementService.kt

---
+++ src/main/kotlin/com/nitramite/gamescoreapi/service/AchievementService.kt
@@ -0,0 +1,49 @@
+package com.nitramite.gamescoreapi.service
+
+import com.nitramite.gamescoreapi.dao.AchievementEntity
+import com.nitramite.gamescoreapi.dao.AchievementTypeEntity
+import com.nitramite.gamescoreapi.model.Achievement
+import com.nitramite.gamescoreapi.model.AchievementType
+import com.nitramite.gamescoreapi.repository.AchievementRepository
+import com.nitramite.gamescoreapi.repository.AchievementTypeRepository
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class AchievementService(
+    private val achievementRepository: AchievementRepository,
+    private val achievementTypeRepository: AchievementTypeRepository
+) {
+
+    fun addAchievementType(name: String, decsription: String): AchievementType {
+        val newAchievementType = AchievementTypeEntity(null, name, decsription)
+        achievementTypeRepository.save(newAchievementType)
+        return AchievementType.fromDao(newAchievementType)
+    }
+
+    fun addAchievement(gameId: Long, clientUid: String, achievementTypeId: Long): Achievement {
+        val newAchievement = AchievementEntity(null, gameId, clientUid, achievementTypeId)
+        achievementRepository.save(newAchievement)
+        return Achievement.fromDao(newAchievement)
+    }
+
+    fun getAchievementsByGameId(gameId: Long): ArrayList<Achievement> {
+        val achievementEntities: List<AchievementEntity> = achievementRepository.findAllByGameId(gameId)
+        val achievements: ArrayList<Achievement> = ArrayList()
+        achievementEntities.forEach {
+            achievements.add(Achievement.fromDao(it))
+        }
+        return achievements
+    }
+
+    fun getAchievementsByGameIdAndClientUid(gameId: Long, clientUid: String): ArrayList<Achievement> {
+        val achievementEntities: List<AchievementEntity> =
+            achievementRepository.findAllByGameIdAndClientUid(gameId, clientUid)
+        val achievements: ArrayList<Achievement> = ArrayList()
+        achievementEntities.forEach {
+            achievements.add(Achievement.fromDao(it))
+        }
+        return achievements
+    }
+
+}
